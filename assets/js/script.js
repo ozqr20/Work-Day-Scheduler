b63a0bfe3acb5df7851cce0f8220329fc480fbb7
Oscar <53874145+ozqr20@users.noreply.github.com>
2022-07-10 16:46:05 -0400

    jumbo date

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+
+var todayDate = function(){
+    // var today = $(find("lead").text().trimp());
+
+    // var day = today.format("DD");
+    // var year = today.format("YYYY");
+    // var month = today.format("MM");
+
+    // console.log(moment(today));
+var now = moment();
+moment.format("DD,YYYY,Mo,MM");
+now = $(find("p").text().trimp());
+console.log(now);
+}
+
+
+
