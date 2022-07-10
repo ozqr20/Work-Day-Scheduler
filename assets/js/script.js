b031bdc8b1fd3844a47ca63cfcaf8e6c1b66c993
Oscar <53874145+ozqr20@users.noreply.github.com>
2022-07-10 17:10:19 -0400

    today's date

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,8 @@
 
-var todayDate = function(){
-    // var today = $(find("lead").text().trimp());
+$(document).ready(function(){
+    $("#currentDay").text(moment().format("dddd, MMMM Do"));
+});
 
-    // var day = today.format("DD");
-    // var year = today.format("YYYY");
-    // var month = today.format("MM");
-
-    // console.log(moment(today));
-var now = moment();
-moment.format("DD,YYYY,Mo,MM");
-now = $(find("p").text().trimp());
-console.log(now);
-}
 
 
 
