12f5133284a0780769f6d29226c8175b98348a40
Oscar <53874145+ozqr20@users.noreply.github.com>
2022-07-10 23:47:48 -0400

    checkTime Func

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,31 @@
+var tasks = {};
 
 $(document).ready(function(){
     $("#currentDay").text(moment().format("dddd, MMMM Do"));
 });
+
+var checkTime = function(timeEl){
+
+    var date = new Date.now()
+    var hour = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    var time = "AM";
+    total = hour + minutes + seconds;
+
+    if(date > total) {
+        console.log(date);
+        alert("it has passed");
+    }
+    else if(date === total){
+        console.log(date);
+        alert("its current")
+    } else{
+        console.log(date);
+        alert("its future")
+    }
+}
+checkTime(10);
 
 
 
