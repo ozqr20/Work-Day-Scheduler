20b1d18ce303b1a71dcdee67778d96bffb7dc79e
Oscar <53874145+ozqr20@users.noreply.github.com>
2022-07-11 01:26:27 -0400

    checkTime loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,24 @@
-var tasks = {};
+
 
 $(document).ready(function(){
     $("#currentDay").text(moment().format("dddd, MMMM Do"));
 });
 
-var checkTime = function(timeEl){
+var checkTime = function(){
 
-    var date = new Date.now()
-    var hour = date.getHours();
-    var minutes = date.getMinutes();
-    var seconds = date.getSeconds();
-    var time = "AM";
-    total = hour + minutes + seconds;
 
-    if(date > total) {
-        console.log(date);
-        alert("it has passed");
-    }
-    else if(date === total){
-        console.log(date);
-        alert("its current")
-    } else{
-        console.log(date);
-        alert("its future")
+    for(var i = 9; i <= 17; i++){
+        if($.now() < i){
+            $("#hour-").addClass(".past");
+        }
+        if($.now()  === i){
+            $("#hour-").addClass(".present");
+        }
+        if ($.now()  > i) {
+            $("hour-").addClass(".future");
+        }
     }
 }
-checkTime(10);
 
 
 
