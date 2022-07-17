be452c99f0ff19c09dfb75b191a5bf02cf710b30
Oscar <53874145+ozqr20@users.noreply.github.com>
2022-07-17 12:38:22 -0400

    color bug fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,7 +22,7 @@ $(document).ready(function(){
         
         $(".time-block").each(function(){
             // point to the attribute id then separate hours with the id "hour" by explicit spliting it and change the string into an integer
-            var timeFrame = parseInt($(this).attr("id").split('hour')[2]);
+            var timeFrame = parseInt($(this).attr("id").split('hour')[1]);
             console.log("timeFrame check" + timeFrame);
 
             //depending on the time the color will be change/remove 
@@ -46,15 +46,15 @@ $(document).ready(function(){
     }; // end checkTime
 
     // adding the id and the user input into the local storage
-    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
-    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
-    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
-    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
-    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
-    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
-    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
-    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
-    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
 
     checkTime();
 
